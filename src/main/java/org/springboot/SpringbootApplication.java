2bbbe65e336569f9e616f916773583c25c70346e
Linag Lo <ll9420416@outlook.com>
2017-11-01 09:24:06 +0800

    update

--- src/main/java/org/springboot/SpringbootApplication.java
+++ src/main/java/org/springboot/SpringbootApplication.java
@@ -3,6 +3,11 @@ package org.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 应用主入口
+ *
+ * @author luoliang
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
